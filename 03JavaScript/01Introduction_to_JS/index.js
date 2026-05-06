254db8b8d9d5f18df4a99fbf319ac549d8c22525
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-06 23:42:44 +0530

    first commit

---
+++ 03JavaScript/01Introduction_to_JS/index.js
@@ -0,0 +1,18 @@
+// console.log("Hello Coder Army");
+
+// let num=10;
+// console.log(num);
+
+// let ar="Joy";
+// ar="Rogit Negi";
+// console.log(ar);
+
+
+// const id=20;
+// console.log(id);
+
+// var nam="Joy";
+// var nam="Rohit";
+// console.log(nam);
+
+

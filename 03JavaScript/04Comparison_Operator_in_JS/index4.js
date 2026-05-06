6b2863cc1e57a8d71684453187c4e79d9d27f921
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-07 02:02:57 +0530

    fourth commit

---
+++ 03JavaScript/04Comparison_Operator_in_JS/index4.js
@@ -0,0 +1,50 @@
+// let a1=1;
+// let a2=2;
+// console.log(a1==a2);
+
+
+// let num=10;
+// let str="10";
+// console.log(num===str);
+
+
+// console.log(null==undefined);
+
+
+// console.log(null===undefined);
+
+
+// console.log(null==0);
+// console.log(null<0);
+// console.log(null>0);
+// console.log(null<=0);
+// console.log(null>=0);
+
+
+
+// console.log(undefined==0);
+// console.log(undefined<0);
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined<=0);
+
+
+// console.log(NaN==NaN);
+
+// let abc1=123;
+// let abc2="123";
+// let abc3=123;
+// console.log(abc1==abc2==abc3);
+
+
+
+// Bitwise operator
+// console.log(4&5);
+
+// console.log(4|5);
+
+// console.log(4^5);
+
+// console.log(5<<3);
+
+// console.log(20>>2);

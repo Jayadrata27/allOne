d8526b9d10b9b4fc4a775f79f7c7bdaa6a8e8827
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-06 23:50:31 +0530

    second commit

---
+++ 03JavaScript/02Datatypes_in_JS/index2.js
@@ -0,0 +1,40 @@
+// premitive data type
+// number
+// let num=10;
+// console.log(num);
+// console.log(typeof num);
+
+
+// string
+// let str="Joy";
+// console.log(str);
+// console.log(typeof str);
+
+
+// Bolean
+// let bool=true;
+// console.log(bool);
+// console.log(typeof bool);
+
+
+// biging
+// let num=12345678963215478444588511255n;
+// console.log(num);
+// console.log(typeof num);
+
+
+
+// null
+// let nam=null;
+// console.log(nam);
+// console.log(typeof nam);
+
+
+
+// undefine
+// let und;
+// console.log(und);
+// console.log(typeof und);
+
+
+

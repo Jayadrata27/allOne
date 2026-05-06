845bba5c4618232a0e73e54da7bfa052e96bbd99
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-07 01:48:32 +0530

    thirdlast commit

--- 03JavaScript/03Operators_in_JS/index3.js
+++ 03JavaScript/03Operators_in_JS/index3.js
@@ -1,4 +1,102 @@
 // Non primitive data type
 // Array
-let arr=[10,20,30,"Rohit","Joy","Ajay"];
-console.log(arr);
+// let arr=[10,20,30,"Rohit","Joy","Ajay"];
+// console.log(arr);
+
+
+
+// Object
+// let obj={
+//     user_name:"Joy",
+//     account_number:41911704780,
+//     account_balence:500
+// }
+// console.log(obj);
+
+
+
+// function
+// let fun=function(){
+//     console.log("Hello Coders");
+// }
+// fun();
+
+
+
+// let fun=function(){
+//     console.log("Hello coders");
+//     return 10;
+// }
+// console.log(fun());
+
+
+
+// Type conversion
+// string to number
+// let str="10";
+// let num=Number(str);
+// console.log(num);
+// console.log(typeof num);
+
+
+
+// Boolean to number
+// let bool=true;
+// let num=Number(bool);
+// console.log(num);
+// console.log(typeof num);
+
+
+// null to number
+// let nal=null;
+// let num=Number(nal);
+// console.log(num);
+// console.log(typeof num);
+
+
+
+// undefined to number
+// let und;
+// let num=Number(und);
+// console.log(num);
+// console.log(typeof num);
+
+
+// number to string
+// let num=123;
+// let str=String(num);
+// console.log(str);
+// console.log(typeof str);
+
+
+// boolean to string
+// let bul=true;
+// let str=String(bul);
+// console.log(str);
+// console.log(typeof str);
+
+
+// String to boolean
+// let str="Joy";
+// let bool=Boolean(str);
+// console.log(bool);
+// console.log(typeof bool);
+
+
+
+// Arithmetic operator
+// Increament or Decrement operator
+
+// let sum=20;
+// // console.log(sum++);
+// console.log(++sum);
+
+
+// let sum=20;
+// // console.log(sum--);
+// console.log(--sum);
+
+
+
+
+

e78c858d8ef51a3638efb5710fa3afea9899c8db
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-07 16:26:03 +0530

    math commit

---
+++ 03JavaScript/07Maths_in_JS/index7.js
@@ -0,0 +1,73 @@
+// toString
+// let num=100;
+// console.log(num.toString());
+
+
+//toExponential
+// let num=123.68;
+// console.log(num.toExponential(2));
+
+
+//toFixed
+// let num=123.689;
+// console.log(num.toFixed(1));
+
+
+//toPrecision
+// let num=123.689;
+// console.log(num.toPrecision(5));
+
+
+//valueOf
+// let num=123.689;
+// console.log(num.valueOf());
+
+
+
+//Math
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.LOG10E);
+
+
+
+// let num=123.45;
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+
+
+
+// console.log(Math.random());
+
+// 0 to 9
+// console.log(Math.floor(Math.random()*10));
+
+//0 to 10
+// console.log(Math.floor(Math.random()*11));
+
+//1 to 12
+// console.log(Math.floor(Math.random()*12)+1);
+
+
+// 40 to 50
+// console.log(Math.floor(Math.random()*10)+40);
+
+
+//Math.random()*(max-min+1)+min
+//50 to 70
+// console.log(Math.floor(Math.random()*(70-50+1)+50));
+
+
+
+
+
+
+
+
+
+
+
+
+
+

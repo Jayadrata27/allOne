ab4f545b7c1aa26324d2673055162b2153cadbe9
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-07 14:26:56 +0530

    sixth commit

---
+++ 03JavaScript/06String_in_JS/index6.js
@@ -0,0 +1,65 @@
+// Declaring String
+// Double Quotes
+// console.log("Hello Coders");
+
+// Single quotes
+// console.log('Coder Army');
+
+// Template literals
+// console.log(`Rohit Bhaiya`);
+
+// length
+// let nam="Rohit";
+// console.log(nam.length);
+
+
+// Accessing Characters
+// let nam="Joy";
+// console.log(nam.charAt(2));
+
+
+// Changing Case
+// let nam="Jayadrata Roy";
+// console.log(nam.toUpperCase());
+// console.log(nam.toLowerCase());
+
+
+
+// Searching in String
+// let nam="Hello I am Rohit Negi am";
+// console.log(nam.indexOf("Rohit"));
+// console.log(nam.lastIndexOf("am"));
+// console.log(nam.includes("Rohit"));
+
+
+
+// Extracting substring
+// let nam="Hello I am Jayadrata Roy.I am a Boy";
+// console.log(nam.slice(1,4));
+// console.log(nam.substring(1,4));
+// console.log(nam.substr(1,3));  
+
+
+
+// Replacing content 
+// let nam="Hello I am Rohit Negi am";
+// console.log(nam.replace("Hello","Ji"));
+// console.log(nam.replaceAll("am","Ji"));
+
+
+
+
+//Splitting String
+// let nam="Hello I am Rohit Negi am";
+// console.log(nam.split());
+
+
+// Trimming
+// let nam="           Hello I am Rohit Negi am";
+// console.log(nam.trim());
+
+
+
+
+
+

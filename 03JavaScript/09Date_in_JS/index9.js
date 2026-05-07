a52b1a3cbef0ebdfa7a5aadf4eed52a52ce0a300
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-07 20:14:06 +0530

    nineth commit

---
+++ 03JavaScript/09Date_in_JS/index9.js
@@ -0,0 +1,70 @@
+// const d=new Date();
+// console.log(d);
+// console.log(d.toDateString());
+// console.log(d.toString());
+// console.log(d.toISOString());
+// console.log(typeof d);
+
+
+
+// const d=new Date(1000);
+// console.log(d);
+
+
+
+// const d=new Date();
+// console.log(d.getDate());
+// console.log(d.getDay());
+// console.log(d.getMonth());
+// console.log(d.getFullYear());
+// console.log(d.getMilliseconds());
+// console.log(d.getMinutes());
+// console.log(d.getTime());
+
+
+
+// const now=Date.now();
+// console.log(now);
+
+
+
+// const d=new Date("2022-10-22");
+// console.log(d);
+// console.log(d.toDateString());
+
+
+
+// const d=new Date("2022-10-20T10:10:12");
+// console.log(d.toDateString());
+
+
+// const date=new Date(2024,4,28);
+// console.log(date.toString());
+
+
+// const date=new Date();
+// date.setDate(20);
+// date.setMonth(3);
+// date.setFullYear(2021);
+// console.log(date);
+
+
+// const date1=new Date();
+// const date2=new Date("2028-07-14T00:00:00");
+// const date=date2-date1;
+// let days=Math.floor(date/(1000*60*60*24));
+// console.log(days);
+
+
+
+// const date1=new Date();
+// const date2=new Date("2028-07-14T00:00:00");
+// const date=date2-date1;
+// const days=Math.floor(date/(1000*60*60*24));
+// const hour=Math.floor(date/(1000*60*60)%24);
+// const minit=Math.floor(date/(1000*60)%60);
+// const second=Math.floor(date/(1000)%60);
+// console.log(`Olympics Count down Time: ${days}days ${hour}hour ${minit}minit ${second}second`);
+
+
+

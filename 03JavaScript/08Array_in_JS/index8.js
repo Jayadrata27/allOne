3588d60c38987aa4c122c27679e8cb8caa5f12c2
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-07 16:49:33 +0530

    eight commit

---
+++ 03JavaScript/08Array_in_JS/index8.js
@@ -0,0 +1,83 @@
+// length
+// let arr=[10,20,30,"Joy","Ajay","Bijoy"];
+// console.log(arr.length)
+
+
+//push
+// let arr=[10,20,30,"Joy","Ajay","Bijoy"];
+// arr.push("Kaku");
+// console.log(arr);
+
+
+// pop
+// let arr=[10,20,30,"Joy","Ajay","Bijoy"];
+// arr.pop();
+// console.log(arr);
+
+
+// unshift
+// let arr=[10,20,30,"Joy","Ajay","Bijoy"];
+// arr.unshift("101");
+// console.log(arr);
+
+
+// shift
+// let arr=[10,20,30,"Joy","Ajay","Bijoy"];
+// arr.shift();
+// console.log(arr);
+
+
+// indexOf
+// let arr=[10,20,30,"Joy","Ajay","Bijoy"];
+// console.log(arr.indexOf(10));
+
+
+
+//includes
+// let arr=[10,20,30,"Joy","Ajay","Bijoy"];
+// console.log(arr.includes("Joy"));
+
+
+
+//slice
+// let arr=[10,20,30,"Joy","Ajay","Bijoy"];
+// console.log(arr.slice(1,5));
+
+
+// splice
+// let arr=[10,20,30,"Joy","Ajay","Bijoy"];
+// console.log(arr.splice(1,2,"Kaku"));
+// console.log(arr);
+
+
+//toString
+// let arr=[10,20,30,"Joy","Ajay","Bijoy"];
+// console.log(arr.toString());
+
+
+//at
+// let arr=[10,20,30,"Joy","Ajay","Bijoy"];
+// console.log(arr.at(1));
+
+
+//join
+// let arr=[10,20,30,"Joy","Ajay","Bijoy"];
+// console.log(arr.join(" ! "));
+
+
+
+// concat
+// let nam1="Joy";
+// let nam2="Roy";
+// console.log(nam1.concat(nam2));
+
+
+// flat
+// let arr=[[1,2],[3,4],[5,6]];
+// console.log(arr.flat(Infinity));
+
+
+
+// let abc=[1,2,3,4];
+// console.log(Array.isArray(abc));
+

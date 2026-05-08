466634ed3253c7e0905b258e9efee1cb5bea1d6a
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-08 23:21:04 +0530

    tenth commit

---
+++ 03JavaScript/10Object_in_JS/index10.js
@@ -0,0 +1,97 @@
+// const obj={
+//     name:"Rohit",
+//     account_balence:1000,
+//     gender:"Male",
+//     age:21
+// }
+// console.log(obj);
+// console.log(obj.name);
+// console.log(obj["age"]);
+
+
+
+// let obj={
+//     0:20,
+//     account_balence:1200,
+//     1:50
+// }
+// console.log(obj);
+// console.log(obj[0]);
+
+
+
+// Create an object
+// let person=new Object();
+// person.name="Joy";
+// person.age=21;
+// person.gender="Male";
+// console.log(person);
+// delete person.name;
+// console.log(person);
+
+
+
+
+// constructor
+// class people{
+//     constructor(name,age){
+//         this.name=name,
+//         this.age=age
+//     }
+// }
+// let p1=new people("Joy",21);
+// console.log(p1);
+
+
+
+
+//keys,values,entries
+// let obj={
+//     name:"Joy",
+//     age:21,
+//     gender:"male"
+// }
+// let arr1=Object.keys(obj);
+// console.log(arr1);
+// let arr2=Object.values(obj);
+// console.log(arr2);
+// let arr3=Object.entries(obj);
+// console.log(arr3);
+
+
+
+
+//assign operator
+// const obj1={
+//     a:10,
+//     b:20
+// }
+// const obj2={
+//     c:30,
+//     d:40
+// }
+// const obj3=Object.assign({},obj1,obj2);
+// console.log(obj3,obj1);
+
+
+
+
+// Spread Operator
+// let obj1={
+//     a:10,
+//     b:20
+// }
+// let obj2={
+//     c:30,
+//     d:40
+// }
+// let obj3={
+//     e:50,
+//     f:60
+// }
+// let obj4={
+//     ...obj1,
+//     ...obj2,
+//     ...obj3
+// }
+// console.log(obj4);

67c8a8df334951f53c47bedfeeffb7779ff0addd
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-09 18:42:16 +0530

    twelve commit

---
+++ 03JavaScript/12Function_in_JS/index12.js
@@ -0,0 +1,93 @@
+// function greet(){
+//     console.log("Hello Coder Army");
+//     console.log("Mein to badiya Hu");
+//     console.log("Aur Kya chal raha hai");
+// }
+// greet();
+
+
+
+// sum
+// function sum(num1,num2){
+//     console.log(num1+num2);
+// }
+// sum(5,6);
+
+
+// function sum(num1,num2){
+//     return num1+num2;
+// }
+// console.log(sum(4,5));
+
+
+
+//Multiply
+// function multiply(num1,num2){
+//     console.log(num1*num2);
+// }
+// multiply(5,4);
+
+
+// function multiply(num1,num2){
+//     return num1*num2;
+// }
+// console.log(multiply(4,5));
+
+
+
+
+// const fun=function(){
+//     console.log("Hello Coder Army");
+//     console.log("Mein to badiya hu");
+//     return 20;
+// }
+// console.log(fun());
+
+
+
+// Arrow Function
+// let fun=()=>{
+//     console.log("I can do everything");
+// }
+// fun();
+
+
+// let fun=(num1,num2)=>num1+num2;
+// console.log(fun(4,5));
+
+
+// let cube=(num)=>num*num*num;
+// console.log(cube(5));
+
+
+// Rest Operator
+// function sum(...number){
+//    console.log(number);
+// }
+// sum(4,5);
+// sum(4,5,6);
+// sum(7,8,5,6);
+
+
+
+// object+function  means an object destructruing by a function
+// let obj={
+//     name:"Joy",
+//     age:21,
+//     amount:1000
+// }
+// function fun({name,age}){
+//     console.log(name,age);
+// }
+// fun(obj);
+
+
+// let obj={
+//     name:"Joy",
+//     age:21,
+//     amount:1000
+// }
+// function fun(obj1){
+//     console.log(obj1.name,obj1.age);
+// }
+// fun(obj);

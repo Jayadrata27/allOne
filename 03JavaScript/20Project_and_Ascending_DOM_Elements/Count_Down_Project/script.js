c005f6e983051ab477115c158d31e7f2e429a857
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-11 03:11:25 +0530

    countdown commit

---
+++ 03JavaScript/20Project_and_Ascending_DOM_Elements/Count_Down_Project/script.js
@@ -0,0 +1,26 @@
+function timer(){
+    let date1=new Date();
+    let date2=new Date("2028-07-14T00:00:00");
+    let date=date2-date1;
+    let days=Math.floor(date/(1000*60*60*24));
+    let hour=Math.floor(date/(1000*60*60)%24);
+    let minit=Math.floor(date/(1000*60)%60);
+    let second=Math.floor(date/(1000)%60);
+
+let count=`
+  Olympic Games LA28 countDown:
+     ${days} days,
+     ${hour} hour,
+     ${minit} minit,
+     ${second} second
+`  ;
+
+
+let even=document.querySelector(".root");
+even.innerHTML=count;
+
+}
+
+
+
+setInterval(timer,1000);

6c5168b6bde0c392a241cc00085d843fe6d849aa
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-11 04:37:33 +0530

    twenty commit

---
+++ 03JavaScript/20Project_and_Ascending_DOM_Elements/Accessing_DOM_Elements/script.js
@@ -0,0 +1,33 @@
+//  Accessing by CSS Selectors 
+//  single Element
+//   document.querySelector("# ");   //id
+ 
+//   document.querySelector(". ");   //className
+
+// Multiple Element 
+//    document.querySelectorAll(". ")  
+
+
+// Accessing by Tag name
+// document.getElementsByTagName("h1");
+
+
+
+// Accessing using Relationship
+// let list=document.querySelector("li")
+// console.log(list.parentElement);              //parentElement
+// console.log(list.parentNode);                 //parent node
+
+
+
+// let par=document.querySelector("ul");
+// console.log(par.childNodes);
+// console.log(par.childNodes[0]);
+// console.log(par.children);
+// console.log(par.firstChild);
+// console.log(par.firstElementChild);
+// console.log(par.lastChild);
+// console.log(par.lastElementChild);
+
+
+

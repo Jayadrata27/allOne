ff63b430f5b74fa05420f24ed11720683b89780f
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-10 14:10:33 +0530

    acc commit

---
+++ 03JavaScript/16Reduce_sets_and_Maps/index16.js
@@ -0,0 +1,70 @@
+//in reduce always use initialize
+// let arr=[10,20,30,40,50];
+// let result=arr.reduce((acc,curr)=>{
+//     acc=acc+curr;
+//     return acc;
+// },0)
+// console.log(result);
+
+
+// let arr=[10,20,30,40,50];
+// let result=arr.reduce((acc,curr)=>acc+curr,0);
+// console.log(result);
+
+
+
+// let obj={
+//     name:"Joy",
+//     age:21
+// }
+// let result=obj.hasOwnProperty("name");
+// console.log(result);
+
+
+
+// let obj={
+//     name:"Joy",
+//     age:21,
+//     orange:1
+// }
+// let curr="orange";
+// if(obj.hasOwnProperty(curr)){
+//     obj[curr]++;
+// }
+// console.log(obj);
+
+
+
+// count the fruits by using reduce
+// let arr=["orange","apple","banana","orange","apple","banana","orange","grapes"];
+// let result=arr.reduce((acc,curr)=>{
+//    if(acc.hasOwnProperty(curr)){
+//       acc[curr]++;
+//    }
+//    else{
+//     acc[curr]=1;
+//    }
+
+//    return acc;
+
+// },{})
+// console.log(result);
+
+
+
+// let arr=["orange","apple","banana","orange","apple","banana","orange","grapes"];
+// let result=arr.reduce((acc,curr)=>{
+//     acc.hasOwnProperty(curr)? acc[curr]++ : acc[curr]=1;
+//     return acc;
+// },{})
+// console.log(result);
+
+
+
+
+
+
+
+
+
+

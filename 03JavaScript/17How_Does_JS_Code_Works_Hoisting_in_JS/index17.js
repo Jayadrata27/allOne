1671078a27522606d78dfb8513dcfb65f5145bd9
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-10 15:39:16 +0530

    seventeen commit

---
+++ 03JavaScript/17How_Does_JS_Code_Works_Hoisting_in_JS/index17.js
@@ -0,0 +1,32 @@
+// JS: Synchronous Single threade Language
+
+// memory allocation
+//greet:function code
+//code execution
+
+// greet()
+// function greet(){
+//     console.log("Helo World");
+// }
+
+
+
+// memory allocation
+// meet:
+// code execution phase
+// meet();
+// let meet=function(){
+//     console.log("Hello World");
+// }
+
+
+
+// memory allocation
+// meet:function chala jaayega
+//codee execution phase
+// let meet=function(){
+//     console.log("Hello Meet");
+// }
+// meet();
+
+

921bb87d61aa36fa1e2cb88d5a1d7d966a0503de
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-16 00:07:48 +0530

    Async commit

---
+++ 03JavaScript/29Async_Task_Working_and_Web_API/index29.js
@@ -0,0 +1,76 @@
+// console.log("Hello Coder Army");
+
+// function meet(){
+//     const arr=[2,4,6];
+//     console.log(arr[0]);
+// }
+
+// function greet(){
+//     const a=2+3;
+//     console.log(a);
+//     meet();
+//     console.log(a*a);
+// }
+
+// greet();
+// console.log("Program End"); 
+
+
+
+
+
+
+
+// console.log("Hello Coder Army");
+
+// setTimeout(()=>{                    //setTimeout is a part of Web Api
+//     const a=2+4;
+//     console.log(a);
+// },5000)
+
+// let b=20;
+// let arr=[20,30,11];
+
+// for(i of arr){
+//     console.log(i*b);
+// }
+
+
+
+
+
+// console.log("HEllo Coder Army");
+
+// setTimeout(()=>{
+//   const a = 2+4;
+//   console.log(a);
+// },5000);
+
+// setInterval(() => {
+//     console.log("I am fast");
+// }, 2000);
+
+// let b = 20;
+// let arr = [20,30,11];
+
+// for(let i of arr)
+//     console.log(i*b);
+
+
+
+
+
+
+console.log("I am First");
+
+fetch("https://youtube.com")
+.then(()=>console.log("Hello"));
+
+console.log("I am last");
+
+
+
+
+
+
+

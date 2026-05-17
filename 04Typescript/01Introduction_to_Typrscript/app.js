022d475c930348173e460ceb58f36c3959e0a82b
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-17 10:18:19 +0530

    introduction typescript

---
+++ 04Typescript/01Introduction_to_Typrscript/app.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// number
+let a = 10;
+// String
+let b = "Joy";
+// bigint
+let c = 12345678963258741233697415899412n;
+// boolean
+let d = true;
+// undefined
+let e;
+// null
+let f = null;
+//# sourceMappingURL=app.js.map
---
+++ 04Typescript/01Introduction_to_Typrscript/app.ts
@@ -0,0 +1,19 @@
+// number
+let a:number=10;
+
+// String
+let b:string="Joy";
+
+// bigint
+let c:bigint=12345678963258741233697415899412n;
+
+// boolean
+let d:boolean=true;
+
+// undefined
+let e:undefined;
+
+// null
+let f:null=null;
+
+

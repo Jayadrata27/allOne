b419d0df94414e7bceae55febe1ecb28dfdd7f91
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-23 17:09:05 +0530

    Props commit

---
+++ 05React/04Myntra_Project_Props/Myntra/myntra.js
@@ -0,0 +1,153 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+//Header
+//Body
+//Footer
+function Card(props){
+   return(
+      <div style={{border:"2px solid black", padding:"2px"}}>
+         <img src={props.imageURL} height="200px" width="200px" />
+         <div style={{textAlign:"center"}}>
+            <h2>{props.cloth}</h2>
+            <h1>{props.offer}</h1>
+            <h2>Shop Now</h2>
+         </div>
+      </div>
+   )
+}
+const arr= [
+  {
+    cloth: "T-shirt",
+    offer: "20-80% off",
+    image:
+      "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab",
+  },
+  {
+    cloth: "Jeans",
+    offer: "30-70% off",
+    image:
+      "https://images.unsplash.com/photo-1473966968600-fa801b869a1a",
+  },
+  {
+    cloth: "Hoodie",
+    offer: "40-75% off",
+    image:
+      "https://images.unsplash.com/photo-1556821840-3a63f95609a7",
+  },
+  {
+    cloth: "Jacket",
+    offer: "25-60% off",
+    image:
+      "https://images.unsplash.com/photo-1523398002811-999ca8dec234",
+  },
+  {
+    cloth: "Shirt",
+    offer: "15-55% off",
+    image:
+      "https://images.unsplash.com/photo-1603252109303-2751441dd157",
+  },
+  {
+    cloth: "Kurta",
+    offer: "35-65% off",
+    image:
+      "https://images.unsplash.com/photo-1622445275576-721325763afe",
+  },
+  {
+    cloth: "Blazer",
+    offer: "50-80% off",
+    image:
+      "https://images.unsplash.com/photo-1594938298603-c8148c4dae35",
+  },
+  {
+    cloth: "Sweater",
+    offer: "20-50% off",
+    image:
+      "https://images.unsplash.com/photo-1576566588028-4147f3842f27",
+  },
+  {
+    cloth: "Track Pant",
+    offer: "10-45% off",
+    image:
+      "https://images.unsplash.com/photo-1517841905240-472988babdf9",
+  },
+  {
+    cloth: "Shorts",
+    offer: "25-55% off",
+    image:
+      "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f",
+  },
+  {
+    cloth: "Saree",
+    offer: "30-75% off",
+    image:
+      "https://images.unsplash.com/photo-1496747611176-843222e1e57c",
+  },
+  {
+    cloth: "Lehenga",
+    offer: "40-85% off",
+    image:
+      "https://images.unsplash.com/photo-1614252235316-8c857d38b5f4",
+  },
+  {
+    cloth: "Formal Pant",
+    offer: "20-60% off",
+    image:
+      "https://images.unsplash.com/photo-1507679799987-c73779587ccf",
+  },
+  {
+    cloth: "Skirt",
+    offer: "35-70% off",
+    image:
+      "https://images.unsplash.com/photo-1581044777550-4cfa60707c03",
+  },
+  {
+    cloth: "Top",
+    offer: "15-65% off",
+    image:
+      "https://images.unsplash.com/photo-1483985988355-763728e1935b",
+  },
+  {
+    cloth: "Coat",
+    offer: "45-80% off",
+    image:
+      "https://images.unsplash.com/photo-1541099649105-f69ad21f3246",
+  },
+  {
+    cloth: "Blouse",
+    offer: "20-50% off",
+    image:
+      "https://images.unsplash.com/photo-1496747611176-843222e1e57c",
+  },
+  {
+    cloth: "Denim Jacket",
+    offer: "30-70% off",
+    image:
+      "https://images.unsplash.com/photo-1512436991641-6745cdb1723f",
+  },
+  {
+    cloth: "Polo T-shirt",
+    offer: "25-60% off",
+    image:
+      "https://images.unsplash.com/photo-1527719327859-c6ce80353573",
+  },
+  {
+    cloth: "Ethnic Wear",
+    offer: "50-90% off",
+    image:
+      "https://images.unsplash.com/photo-1617038220319-276d3cfab638",
+  },
+];
+function App(){
+    return(
+        // Header
+        // Body
+        <div style={{display:"flex",gap:"20px",flexWrap:"wrap"}}>
+            {
+                arr.map((value,index)=> <Card key={index} imageURL={value.image} cloth={value.cloth} offer={value.offer}/>)
+            }
+        </div>
+        // Footer
+    )
+}
+const Root=ReactDOM.createRoot(document.getElementById("root"));
+Root.render(<App/>);

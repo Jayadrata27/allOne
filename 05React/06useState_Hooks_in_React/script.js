359b14f14c7af1e37e47a7d70685fcf8e2902d3f
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-24 07:00:06 +0530

    useStateHook commit

---
+++ 05React/06useState_Hooks_in_React/script.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react"
+import ReactDOM from "react-dom/client"
+
+function Counter(){
+    let [count,setCount]=useState(0);
+
+    function incrementnumber(){
+        count=count+1;
+        setCount(count);
+        console.log(`count value is: ${count}`)
+    }
+
+    function decrementnumber(){
+        count=count-1
+        setCount(count);
+        console.log(`Count value is ${count}`);
+        
+    }
+
+    return(
+        <div className="first">
+            <h1>Count is: {count}</h1>
+            <button onClick={incrementnumber}>Increment {count}</button>     
+            <button onClick={decrementnumber}>Decrement {count}</button>     
+        </div>
+        
+    )
+}
+
+const Root=ReactDOM.createRoot(document.getElementById("root"));
+Root.render(<Counter/>)

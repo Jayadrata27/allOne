77fc8e7bef22e9fcaf7548dd3d5adbd8aead08eb
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-24 06:58:35 +0530

    useState Hook

--- 05React/05Component_in_React/src/Myntra.js
+++ 05React/05Component_in_React/src/Myntra.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client"
 import Card from "./Component/Card";
 import Header from "./Component/Header";
@@ -11,12 +11,29 @@ import { greet, meet } from "./utlities/dummy";    //jakhon akta file theke duba
 
 
 function App(){
+ 
+    let [Ar,setAr]=useState(arr);
+
+    function sortArray(){
+        Ar.sort((a,b)=>a.price-b.price);
+        setAr([...Ar]);
+    }
+    function priceAbove1500(){
+       const B=arr.filter((value)=>value.price>1500);
+       setAr(B);
+    }
+
+
     return(
         <>
         <Header/>
+
+        <button onClick={sortArray}>Sort by Price</button>
+        <button onClick={priceAbove1500}>Price above 1500</button>
+
         <div className="middle" style={{display:"flex",gap:"20px",flexWrap:"wrap"}}>
             {
-                arr.map((value,index)=> <Card key={index} imageURL={value.image} cloth={value.cloth} offer={value.offer}/>)
+                Ar.map((value,index)=> <Card key={index} imageURL={value.image} cloth={value.cloth} offer={value.offer} price={value.price} />)
             }
         </div>
         <Footer/>
--- 05React/05Component_in_React/src/utlities/dummy.js
+++ 05React/05Component_in_React/src/utlities/dummy.js
@@ -2,120 +2,141 @@ const arr= [
   {
     cloth: "T-shirt",
     offer: "20-80% off",
+    price:2500,
     image:
       "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab",
+
   },
   {
     cloth: "Jeans",
     offer: "30-70% off",
+    price:1100,
     image:
       "https://images.unsplash.com/photo-1473966968600-fa801b869a1a",
   },
   {
     cloth: "Hoodie",
     offer: "40-75% off",
+    price:1200,
     image:
       "https://images.unsplash.com/photo-1556821840-3a63f95609a7",
   },
   {
     cloth: "Jacket",
     offer: "25-60% off",
+    price:1300,
     image:
       "https://images.unsplash.com/photo-1523398002811-999ca8dec234",
   },
   {
     cloth: "Shirt",
     offer: "15-55% off",
+    price:1400,
     image:
       "https://images.unsplash.com/photo-1603252109303-2751441dd157",
   },
   {
     cloth: "Kurta",
     offer: "35-65% off",
+    price:1900,
     image:
       "https://images.unsplash.com/photo-1622445275576-721325763afe",
   },
   {
     cloth: "Blazer",
     offer: "50-80% off",
+    price:1600,
     image:
       "https://images.unsplash.com/photo-1594938298603-c8148c4dae35",
   },
   {
     cloth: "Sweater",
     offer: "20-50% off",
+    price:1700,
     image:
       "https://images.unsplash.com/photo-1576566588028-4147f3842f27",
   },
   {
     cloth: "Track Pant",
     offer: "10-45% off",
+    price:1800,
     image:
       "https://images.unsplash.com/photo-1517841905240-472988babdf9",
   },
   {
     cloth: "Shorts",
     offer: "25-55% off",
+    price:1500,
     image:
       "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f",
   },
   {
     cloth: "Saree",
     offer: "30-75% off",
+    price:2300,
     image:
       "https://images.unsplash.com/photo-1496747611176-843222e1e57c",
   },
   {
     cloth: "Lehenga",
     offer: "40-85% off",
+    price:2100,
     image:
       "https://images.unsplash.com/photo-1614252235316-8c857d38b5f4",
   },
   {
     cloth: "Formal Pant",
     offer: "20-60% off",
+    price:2200,
     image:
       "https://images.unsplash.com/photo-1507679799987-c73779587ccf",
   },
   {
     cloth: "Skirt",
     offer: "35-70% off",
+    price:2000,
     image:
       "https://images.unsplash.com/photo-1581044777550-4cfa60707c03",
   },
   {
     cloth: "Top",
     offer: "15-65% off",
+    price:2400,
     image:
       "https://images.unsplash.com/photo-1483985988355-763728e1935b",
   },
   {
     cloth: "Coat",
     offer: "45-80% off",
+    price:1000,
     image:
       "https://images.unsplash.com/photo-1541099649105-f69ad21f3246",
   },
   {
     cloth: "Blouse",
     offer: "20-50% off",
+    price:2600,
     image:
       "https://images.unsplash.com/photo-1496747611176-843222e1e57c",
   },
   {
     cloth: "Denim Jacket",
     offer: "30-70% off",
+    price:2700,
     image:
       "https://images.unsplash.com/photo-1512436991641-6745cdb1723f",
   },
   {
     cloth: "Polo T-shirt",
     offer: "25-60% off",
+    price:2800,
     image:
       "https://images.unsplash.com/photo-1527719327859-c6ce80353573",
   },
   {
     cloth: "Ethnic Wear",
     offer: "50-90% off",
+    price:2900,
     image:
       "https://images.unsplash.com/photo-1617038220319-276d3cfab638",
   },

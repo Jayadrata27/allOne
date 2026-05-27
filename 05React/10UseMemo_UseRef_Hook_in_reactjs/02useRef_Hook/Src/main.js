748ca3353e5ae3ab36625a11adbe396553b3f5ac
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-27 10:18:24 +0530

    useRef

---
+++ 05React/10UseMemo_UseRef_Hook_in_reactjs/02useRef_Hook/Src/main.js
@@ -0,0 +1,28 @@
+import React, { useRef, useState } from "react";
+import ReactDOM from "react-dom/client"
+
+function App(){
+
+    const [count,setCount]=useState(0);
+    let money=useRef(0);
+    // console.log(money.current);
+
+    return(
+        <>
+          <h1>Counter is: {count}</h1>
+          <button onClick={()=>setCount(count+1)}>Increment</button>
+ 
+          
+          <h1>Money is: {money.current}</h1>
+          <button onClick={()=>{
+             money.current=money.current+1;
+             console.log(money.current);
+
+          }}>Increment</button>
+          
+        </>
+    )
+}
+
+const Root=ReactDOM.createRoot(document.getElementById("root"));
+Root.render(<App/>);

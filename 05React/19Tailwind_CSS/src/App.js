8566ca3cab9c36fa42275f4821b4a1950080278b
Jayadrata Roy <royjayadrata@outlook.com>
2026-05-31 22:34:12 +0530

    tailwind css

---
+++ 05React/19Tailwind_CSS/src/App.js
@@ -0,0 +1,121 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+function App(){
+    return(
+        <>
+            <div className="flex flex-wrap justify-center gap-2">
+                 <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://i.pinimg.com/736x/60/a6/e2/60a6e2b0776d1d6735fce5ae7dc9b175.jpg"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Biography</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Joy is very good boy , He is very garib because he operates Ola</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-800">Know More</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://i.pinimg.com/736x/60/a6/e2/60a6e2b0776d1d6735fce5ae7dc9b175.jpg"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Biography</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Joy is very good boy , He is very garib because he operates Ola</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-800">Know More</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://i.pinimg.com/736x/60/a6/e2/60a6e2b0776d1d6735fce5ae7dc9b175.jpg"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Biography</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Joy is very good boy , He is very garib because he operates Ola</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-800">Know More</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://i.pinimg.com/736x/60/a6/e2/60a6e2b0776d1d6735fce5ae7dc9b175.jpg"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Biography</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Joy is very good boy , He is very garib because he operates Ola</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-800">Know More</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://i.pinimg.com/736x/60/a6/e2/60a6e2b0776d1d6735fce5ae7dc9b175.jpg"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Biography</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Joy is very good boy , He is very garib because he operates Ola</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-800">Know More</button>
+                </div>
+            </div>
+
+            <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+                <div>
+                    <img className="w-full h-48 object-cover rounded-2xl" src="https://i.pinimg.com/736x/60/a6/e2/60a6e2b0776d1d6735fce5ae7dc9b175.jpg"></img>
+                </div>
+
+                <div className="mt-2 font-bold text-2xl text-gray-700">
+                    <h1>Biography</h1>
+                </div>
+
+                <div className="text-gray-500 mt-2">
+                    <p>Joy is very good boy , He is very garib because he operates Ola</p>
+                </div>
+
+                <div className="mt-3 flex justify-center">
+                    <button className="bg-sky-500 p-1 rounded-xl text-white text-2xl hover:bg-sky-800">Know More</button>
+                </div>
+              </div>
+            </div>
+        </>
+    )
+}
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>)

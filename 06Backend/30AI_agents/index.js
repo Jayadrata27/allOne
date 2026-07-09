82a748ce8b24368694d39c049f15c552d87ae31e
Jayadrata Roy <royjayadrata@outlook.com>
2026-07-09 20:21:10 +0530

    AI agent

---
+++ 06Backend/30AI_agents/index.js
@@ -0,0 +1,109 @@
+require('dotenv').config();
+const { GoogleGenAI } = require('@google/genai');
+const readlineSync = require('readline-sync');
+ 
+
+
+const ai = new GoogleGenAI({ apiKey: process.env.GCP_API_KEY }); 
+const ConversationHistory=[];
+
+async function main(msg) {
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.5-flash',
+    contents: ConversationHistory,
+  });
+  return response.text;
+}
+
+
+
+
+// Weather leke aayega
+
+async function getWeather(location){
+
+    const weatherInfo=[];
+
+    for(const {city,date} of location){
+
+      if(date.toLowerCase()=="today"){
+          const response=await fetch(`http://api.weatherapi.com/v1/current.json?key=eee5cab4f559470090b14916260907&q=${city}`)
+          const data=await response.json();
+          weatherInfo.push(data);
+      }
+      else{
+          const response=await fetch(`http://api.weatherapi.com/v1/current.json?key=eee5cab4f559470090b14916260907&q=${city}&dt=${date}`)
+          const data=await response.json();
+          weatherInfo.push(data);
+      }
+       
+    }
+
+    return weatherInfo;
+}
+
+
+
+async function chatting(){
+const question = readlineSync.question('How can I help you? ');
+const prompt=`
+You are an AI agent, who will respond to me in JSON format only.
+Analyse the user query and try to fetch city and date details from it.
+Date format should be in (yyyy-month-date) if user ask for future weather.
+If user ask for today weather, mark date as 'today'.
+To fetch weather details, I already have some function which can fetch the weather details for me,
+
+
+if you need weather information, use the below format
+JSON format should look like below:
+{
+  "weather_details_needed":true,
+  "location":[{"city":"mumbai","date":"today"},{"city":"delhi","date":"2025-04-30"}]
+}
+
+Once you have the weather report details, response me in JSON format only.
+If I have provided you weather details of delhi and you have enough information about them, make the summary report and return it to me like below.
+JSON format should look like below:
+{
+  "weather_details_needed":false,
+  "weather_report":"Bhai Delhi ka masum tho badiya hai, 18 degree temperature hai, ghar pe pakode bana lo, maja aayega khane mein",
+}
+
+
+User asked this question:${question}
+
+Strictly follow JSON format, respond only in JSON format, don't add extra space or comment in JSON format, please follow these rule. Thank You
+`
+
+ConversationHistory.push({
+  role:"user",
+  parts:[{text:prompt}]
+})
+
+
+while(true){
+let response=await main();
+ConversationHistory.push({role:'model',parts:[{text:response}]});
+response=response.replace(/^```json\s*```$/g, '').trim();
+const data=JSON.parse(response);
+console.log(data);
+
+if(data.weather_details_needed){
+   console.log(data.weather_report);
+   break;
+}
+
+
+const weatherInformation=await getWeather(data.location);
+const weatherInfo=JSON.stringify(weatherInformation);
+ConversationHistory.push({role:'user',parts:[{text:`This is the weather report I have fetched for you, use this weather report to generate user response${weatherInfo}`}]});
+
+   }
+}
+chatting();
+
+// LLM ko Bolunga:Delhi and Mumbai ka mausam bata, return mein muje location wala array de dena
+// [{city:"Delhi",date:'today'},{city:"Mumbai",date:'today'}];
+// Location gateway-->Actual Weather laake de dega
+// Actual weather aaya hai, LLM ko dunga,iska weather report card ready kar de
+// User output mein show kara dunga
